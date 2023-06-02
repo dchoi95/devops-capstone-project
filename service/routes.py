631ce1a93900ae70a558f92e768e3af3ae5e5af3
dchoi95 <d.choi95@hotmail.com>
2023-06-01 22:24:55 -0400

    completed ci build

--- service/routes.py
+++ service/routes.py
@@ -64,6 +64,7 @@ def create_accounts():
 # ... place you code here to LIST accounts ...
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
+
     app.logger.info("Request to list Accounts")
     accounts = Account.all()
     account_list = [account.serialize() for account in accounts]
@@ -77,6 +78,7 @@ def list_accounts():
 # ... place you code here to READ an account ...
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
+    
     app.logger.info("Request to read an Account with id: %s", account_id)
     account = Account.find(account_id)
     if not account:
@@ -91,6 +93,7 @@ def get_accounts(account_id):
 # ... place you code here to UPDATE an account ...
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
+    
     app.logger.info("Request to update an Account with id: %s", account_id)
     account = Account.find(account_id)
     if not account:
