00df024f326928843d8db79df3b2e4091f3f7d8b
dchoi95 <d.choi95@hotmail.com>
2023-06-01 21:31:10 -0400

    cd-retry2

--- service/routes.py
+++ service/routes.py
@@ -80,7 +80,7 @@ def get_accounts(account_id):
     app.logger.info("Request to read an Account with id: %s", account_id)
     account = Account.find(account_id)
     if not account:
-            abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
     return account.serialize(), status.HTTP_200_OK
 
 
@@ -103,7 +103,7 @@ def update_accounts(account_id):
 ######################################################################
 
 # ... place you code here to DELETE an account ...
-@app.route("/accounts/<int:account_id>", methods=["DELETE"])
+@app.route("/accounts/<int:account_id>", gmethods=["DELETE"])
 def delete_accounts(account_id):
     app.logger.info("Request to delete an Account with id: %s", account_id)
     account = Account.find(account_id)
