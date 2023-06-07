5a504cfdd80d3259c7ecb8ddb9f0c18f22af9750
dchoi95 <d.choi95@hotmail.com>
2023-06-07 12:02:13 -0400

    completed ci build

--- service/routes.py
+++ service/routes.py
@@ -104,7 +104,6 @@ def update_accounts(account_id):
 # DELETE AN ACCOUNT
 ######################################################################
 
-
 # ... place you code here to DELETE an account ...
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
