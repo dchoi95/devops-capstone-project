16e0efc2bd6441ff5e605051ac854cb73daf6057
dchoi95 <d.choi95@hotmail.com>
2023-06-07 11:32:15 -0400

    completed ci build

--- service/routes.py
+++ service/routes.py
@@ -64,7 +64,6 @@ def create_accounts():
 
 # ... place you code here to LIST accounts ...
 @app.route("/accounts", methods=["GET"])
-
 def list_accounts():
     app.logger.info("Request to list Accounts")
     accounts = Account.all()
@@ -78,7 +77,6 @@ def list_accounts():
 
 # ... place you code here to READ an account ...
 @app.route("/accounts/<int:account_id>", methods=["GET"])
-
 def get_accounts(account_id):
     app.logger.info("Request to read an Account with id: %s", account_id)
     account = Account.find(account_id)
@@ -94,8 +92,6 @@ def get_accounts(account_id):
 # ... place you code here to UPDATE an account ...
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
-
-
     app.logger.info("Request to update an Account with id: %s", account_id)
     account = Account.find(account_id)
     if not account:
@@ -109,7 +105,6 @@ def update_accounts(account_id):
 
 # ... place you code here to DELETE an account ...
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
-
 def delete_accounts(account_id):
     app.logger.info("Request to delete an Account with id: %s", account_id)
     account = Account.find(account_id)
